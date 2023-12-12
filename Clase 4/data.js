5ce177e04f5846fd47df72fb7acce5b4a0df82e7
Diego <dmpetitto2004@gmail.com>
2023-12-12 15:45:19 -0300

    update exercise

---
+++ Clase 4/data.js
@@ -0,0 +1,65 @@
+const data = [
+    { 
+        id: 1, 
+        name: 'Atomic habits', 
+        author: 'James Clear', 
+        url: 'https://www.google.com.ar/books/edition/Atomic_Habits/fFCjDQAAQBAJ?hl=es-419&gbpv=0' 
+    },
+    { 
+        id: 2, 
+        name: 'The power of positive thinking', 
+        author: 'Norman Vincent Peale',  
+        url: 'https://www.google.com.ar/books/edition/El_poder_del_pensamiento_positivo/pMaLDwAAQBAJ?hl=es-419&gbpv=0' 
+    },
+    { 
+        id: 3, 
+        name: 'The magic of thinking big', 
+        author: 'David Schwartz', 
+        url: 'https://www.google.com.ar/books/edition/The_Magic_of_Thinking_Big/ytB1CwAAQBAJ?hl=es-419&gbpv=0' 
+    },
+    { 
+        id: 4, 
+        name: 'El monje que vendió su Ferrari', 
+        author: 'Robin Sharma', 
+        url: 'https://www.google.com.ar/books/edition/El_monje_que_vendió_su_Ferrari/iBUBhR_pOJ4C?hl=es&gbpv=0' 
+    },
+    { 
+        id: 5, 
+        name: 'Padre rico, padre pobre', 
+        author: 'Robert Kiyosaki', 
+        url: 'https://www.google.com.ar/books/edition/Padre_rico_padre_pobre/jX4jzQEACAAJ?hl=es' 
+    },
+    { 
+        id: 6, 
+        name: 'Piense y hágase rico', 
+        author: 'Napoleon Hill', 
+        url: 'https://www.google.com.ar/books/edition/Piense_y_hágase_rico_Traducción_David/HavEDwAAQBAJ?hl=es&gbpv=0' 
+    },
+    { 
+        id: 7, 
+        name: 'El poder está dentro de ti', 
+        author: 'Louise L. Hay', 
+        url: 'https://www.google.com.ar/books/edition/El_poder_está_dentro_de_ti/VRllQwAACAAJ?hl=es' 
+    },
+    { 
+        id: 8, 
+        name: 'Los 7 hábitos de la gente altamente efectiva', 
+        author: 'Stephen R. Covey', 
+        url: 'https://www.google.com.ar/books/edition/Los_7_hábitos_de_la_gente_altamente_efe/PfWCBgAAQBAJ?hl=es&gbpv=0' 
+    },
+    { 
+        id: 9, 
+        name: 'Mindset: La mentalidad del éxito', 
+        author: 'Carol S. Dweck', 
+        url: 'https://www.google.com.ar/books/edition/Mindset/92ssDwAAQBAJ?hl=es&gbpv=0' 
+    },
+    { 
+        id: 10, 
+        name: 'El código del dinero', 
+        author: 'Raimon Samsó', 
+        url: 'https://www.google.com.ar/books/edition/EL_CÓDIGO_DEL_DINERO/AD8qQAAACAAJ?hl=es' 
+    }
+]
+
+
+module.exports = data;
