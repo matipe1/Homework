6b4bc21a5e8a54033da1a2598ceb89c5e1a170ca
Diego <106128073+matipe1@users.noreply.github.com>
2023-12-12 16:27:00 -0300

    Delete Clase 4 directory

--- Clase 4/middlewares/errorHandler.js
+++
@@ -1,8 +0,0 @@
-// Middleware to handle errors
-
-const errorHandler = (err, req, res, next) => {
-    console.log(err);
-    res.status(err.status || 500).json({ error: err.message || "Server's Problem" });
-};
-
-module.exports = errorHandler;
